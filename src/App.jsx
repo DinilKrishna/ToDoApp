dcc29372d01b4a83b4fc9fc453cf9c6d3815a750
dinil <dinilkrishna46@gmail.com>
2024-02-19 19:28:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Todo from './components/TodoApp/Todo'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      <Todo />
+    </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TodoApp/Todo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <>
+        <div className=''>
+            <div>TODO APP</div>
+            <div>Add Task</div>
+            <div>List Task</div>
+        </div>
+    </>
+  )
+}
+
+export default Todo
